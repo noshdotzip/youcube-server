e7e26bebe66663219ad355ab53a46d4b121e0c07
noshdotzip <majorsboy1@gmail.com>
2026-02-22 00:29:34 -0500

    Update metadata and links for new repo

--- src/youcube/youcube.py
+++ src/youcube/youcube.py
@@ -304,7 +304,7 @@ class CustomErrorHandler(ErrorHandler):
             return text(
                 "You cannot access a YouCube server directly. "
                 "You need the YouCube client. "
-                "See https://youcube.madefor.cc/guides/client/installation/"
+                "See https://github.com/REPLACE_OWNER/youcube-client"
             )
 
         return super().default(request, exception)
